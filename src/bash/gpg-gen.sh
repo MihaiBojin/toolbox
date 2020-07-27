cac4f10aa9944a8eb4415900caa744fea6848661
Mihai Bojin <mihai.bojin@gmail.com>
2020-07-27 21:43:37 +0100

    Added passphrase option

--- src/bash/gpg-gen.sh
+++ src/bash/gpg-gen.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+#
+# Helper script that generates new public/private GPG key pairs
+#
+# Usage:
+#
+# Generate a key for John Doe; you will be prompted for a passphrase
+# gpg-gen.sh --name \"John Doe\" --email john@doe.com" --pass
+#
+# If you want to generate a key without password (insecure)
+# gpg-gen.sh --name \"John Doe\" --email john@doe.com"
+#
+
 set -ueo pipefail
 
 if ! command -v gpg; then
@@ -24,13 +36,37 @@ fi
 # Load arguments
 NAME=
 EMAIL=
+pass=
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
-        --name) NAME=$2; shift 2 ;;
-        --email) EMAIL=$2; shift 2 ;;
+        --name) NAME="$2"; shift 2 ;;
+        --email) EMAIL="$2"; shift 2 ;;
+        --pass) pass="yes"; shift 1 ;;
         * ) echo "Invalid option $1"; _usage ;;
     esac
 done
+
+# Read password
+PASS_HEADER="%no-protection"
+if [[ -n "$pass" ]]; then
+    read -s -r -p "Please enter a password for your GPG key: " PASS1
+    echo
+    read -s -r -p "Enter the password again: " PASS2
+    echo
+    if [[ "$PASS1" != "$PASS2" ]]; then
+        echo "The passwords do not match."
+        echo
+        exit 1
+    fi
+    PASS_HEADER="Passphrase: $PASS1"
+    echo
+    echo "You will be prompted for a password by GPG"
+    echo "Make sure you have configured your pinentry program in ~/.gnupg/gpg-agent.conf, e.g.:"
+    echo "pinentry-program /usr/local/bin/pinentry-mac"
+    echo "OR"
+    echo "pinentry-program /usr/local/bin/pinentry-tty"
+    echo
+fi
 
 echo
 echo "==> Generating a GPG key for $NAME <$EMAIL>..."
@@ -38,7 +74,6 @@ echo "==> Generating a GPG key for $NAME <$EMAIL>..."
 TMPFILE="$(mktemp)"
 (
 echo "%echo Generating a basic OpenPGP key"
-echo "%no-protection"
 echo "Key-Type: DSA"
 echo "Key-Length: 1024"
 echo "Subkey-Type: ELG-E"
@@ -46,6 +81,7 @@ echo "Subkey-Length: 1024"
 echo "Name-Real: $NAME"
 echo "Name-Comment: $NAME"
 echo "Name-Email: $EMAIL"
+echo "$PASS_HEADER"
 echo "Expire-Date: 0"
 echo "%commit"
 ) > "$TMPFILE"
@@ -68,7 +104,7 @@ echo
 echo
 gpg --armor --export "${GPG_KEY}"
 echo
-gpg --armor --export-secret-keys "${GPG_KEY}"
+gpg --armor --export-secret-key "${GPG_KEY}"
 
 # Cleanup
 rm -rf "$GNUPGHOME"
